8aa48d420cf55d4eb8ccb1d3443d3bc170defb60
Marco Gallo <marco.gallo@videocanva.com>
2021-08-08 18:37:59 +0200

    Aggiunta funzione divisione con controllo

--- function.php
+++ function.php
@@ -19,4 +19,16 @@ fuction sottrazione (n1,n2) {
 fuction moltiplicazione (n1,n2) {
     return n1 * n2;
 }
+
+
+<!-- divisione -->
+
+fuction divisione (n1,n2) {
+    if(n2 !== 0){
+        return n1 / n2;}
+        else{
+            return "Errore"
+        }
+}
+
 ?>
